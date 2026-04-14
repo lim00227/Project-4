3094666dee5eaab72c2cf07bb98a3b6da0defe66
lim00227 <lim00227@umn.edu>
2026-04-14 18:55:23 -0500

    Implement line reading function in linked_list.c
    Added a function to read a specific line from a file into a buffer.

--- linked_list.c
+++ linked_list.c
@@ -3,23 +3,31 @@
 #include <string.h>
 #include "pthread.h"
 #include "linked_list.h"
-
+char buffer[256];
 // initialize head
 node *head = NULL;
 
 // sequence counter
 int seq_ctr = 0;
-
+// current node
+node *cur = NULL;
 //Read the file on a line by line basis
 char* read_line(char* fname, int line_no) 
 {
-	// TODO 
+	FILE *fp = fopen(fname, "r");
+	while(fgets(buffer, sizeof(buffer), fp) != NULL) {
+		if (seq_ctr == line_no) {
+			return buffer;
+		}
+		seq_ctr++;
+	}
+	fclose(fp);
 } 
 
 //traverse the linked list
 void traversal(node *head)
 {
-	// TODO
+
 }
 
 // insert the node into the linked list
